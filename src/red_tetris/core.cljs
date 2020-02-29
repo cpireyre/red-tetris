e775dde0430be0958d65097cf6ad6a926569b25f
cpireyre <cpireyre@student.42.fr>
2020-02-29 20:12:52 +0100

    Decluttered tetroes namespace.

--- src/red_tetris/core.cljs
+++ src/red_tetris/core.cljs
@@ -1,1 +1,12 @@
-(ns red-tetris.core)
+(ns red-tetris.core
+  (:require [red-tetris.tetroes :as tetroes]
+            [cljs.core.async :refer [go-loop chan take! onto-chan <! timeout]]))
+
+(let [c (tetroes/random-tetroes 42)
+        tetroes-chan (chan)]
+    (onto-chan tetroes-chan c)
+    (go-loop [i 0]
+             (when (< i 20)
+               (take! tetroes-chan print)
+               (<! (timeout 1000))
+               (recur (inc i)))))
--- src/red_tetris/tetroes.cljs
+++ src/red_tetris/tetroes.cljs
@@ -15,16 +15,6 @@
   (seeded-random-seq tetroes m3-hash-int s))
 
 (comment
-  (:require  [cljs.core.async :refer [go-loop chan take! onto-chan <! timeout]])
-  (let [c (random-tetroes 42)
-          tetroes-chan (chan)]
-      (onto-chan tetroes-chan c)
-      (go-loop [i 0]
-               (when (< i 20)
-                 (take! tetroes-chan print)
-                 (<! (timeout 1000))
-                 (recur (inc i)))))
-  
   (def sss (chan))
   (def bbb (random-tetroes 5))
   (onto-chan sss bbb)
