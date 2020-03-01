523edc8b0059f88a956ade8ae93cb18662865858
cpireyre <cpireyre@student.42.fr>
2020-03-01 12:56:57 +0100

    Trying out memoization.

--- src/red_tetris/tetroes.cljs
+++ src/red_tetris/tetroes.cljs
@@ -17,6 +17,18 @@
 (comment
   (def sss (chan))
   (def bbb (random-tetroes 5))
+  (def ccc (random-tetroes 5))
   (onto-chan sss bbb)
-  (realized? (rest bbb)))
- 
+  (realized? (rest bbb))
+  (realized? bbb) ; false
+  (take 10 bbb) ; now bbb is realized
+  (realized? ccc)) ; but ccc is not
+
+(comment
+  (def random-tetroes-memo (memoize random-tetroes))
+  (def a (random-tetroes-memo 10))
+  (realized? a) ; false
+  (def b (random-tetroes-memo 10))
+  (take 10 a)
+  (realized? a) ; true
+  (realized? b)) ; also true!
